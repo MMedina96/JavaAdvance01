e763a439a6225486419190639b0f9efaae894419
Marcelo Costa Medina - PrestServ <marcelo.c.medina.prestserv@petrobras.com.br>
2024-05-28 18:06:40 -0300

    Commit inicial

---
+++ src/main/java/com/fiap/java/mod5/Java_mod5/JavaMod5Application.java
@@ -0,0 +1,13 @@
+package com.fiap.java.mod5.Java_mod5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMod5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMod5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/java/mod5/Java_mod5/JavaMod5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.java.mod5.Java_mod5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMod5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
